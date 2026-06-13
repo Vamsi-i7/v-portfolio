a6f93a00f2baa0105c0d8140e7ca57480adbb4a0
BALA VAMSI KRISHNA <vaniramesh3484@gmail.com>
2026-06-13 15:35:46 +0530

    fix: auth context update

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,4 +1,3 @@
-import * as React from 'react'
 import { createContext, useContext, useEffect, useState } from 'react'
 import type { Session, User } from '@supabase/supabase-js'
 import { supabase } from '@/lib/supabase'
